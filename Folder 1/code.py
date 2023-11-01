ac9f18c05c21bdaca2c76ffde508ed2d80bbd6e0
onurmikail <onka19ab@student.cbs.dk>
2023-11-01 15:06:25 +0100

    Updated V.2

--- Folder 1/code.py
+++ Folder 1/code.py
@@ -1,7 +1,10 @@
+import pandas as pd
+import numpy as np
+
 # Visualization
 import matplotlib.pyplot as plt
-import pandas as pd
-
+import seaborn as sns
+from matplotlib import colors
 
 %matplotlib inline
 plt.style.use('fivethirtyeight')
@@ -13,7 +16,15 @@ pd.options.display.max_columns = 150
 # Read in data
 train = pd.read_csv('./train.csv')
 test = pd.read_csv('./test.csv')
+
 train.head()
+
+test.info()
 
 train.info()
 
+train.select_dtypes(np.int64).nunique().value_counts().sort_index().plot.bar(color = 'blue',
+                                                                             figsize = (8, 6),
+                                                                             edgecolor = 'k', linewidth = 2);
+plt.xlabel('Number of Unique Values'); plt.ylabel('Count');
+plt.title('Count of Unique Values in Integer Columns');

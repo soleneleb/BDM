afc8dce3a138e489130fb2406917b4e2592dff9c
onur2720 <54842876+onur2720@users.noreply.github.com>
2023-11-02 22:06:55 +0100

    Update code.py working plot

--- Folder 1/code.py
+++ Folder 1/code.py
@@ -1,274 +1,171 @@
-import pandas as pd
-import numpy as np
-
-# Visualization
+import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import seaborn as sns
-from matplotlib import colors
+import warnings
+from collections import OrderedDict
 
+# Ignore FutureWarnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+# Set plot style
 plt.style.use('fivethirtyeight')
 plt.rcParams['font.size'] = 18
 plt.rcParams['patch.edgecolor'] = 'k'
 
+# Increase the maximum number of displayed columns
 pd.options.display.max_columns = 150
 
 # Read in data
 train = pd.read_csv('./train.csv')
 test = pd.read_csv('./test.csv')
 
+# Explore the data
 train.head()
-
 test.info()
-
 train.info()
 
-train.select_dtypes(np.int64).nunique().value_counts().sort_index().plot.bar(color = 'blue',
-                                                                             figsize = (8, 6),
-                                                                             edgecolor = 'k', linewidth = 2);
-plt.xlabel('Number of Unique Values'); plt.ylabel('Count');
-plt.title('Count of Unique Values in Integer Columns');
+# Visualize the count of unique values in integer columns
+train.select_dtypes(np.int64).nunique().value_counts().sort_index().plot.bar(
+    color='blue', figsize=(8, 6), edgecolor='k', linewidth=2)
+plt.xlabel('Number of Unique Values')
+plt.ylabel('Count')
+plt.title('Count of Unique Values in Integer Columns')
 
-from collections import OrderedDict
-
-plt.figure(figsize = (20, 16))
-plt.style.use('fivethirtyeight')
-
-# Color mapping
+# Color mapping and poverty levels
 colors = OrderedDict({1: 'red', 2: 'orange', 3: 'blue', 4: 'green'})
 poverty_mapping = OrderedDict({1: 'extreme', 2: 'moderate', 3: 'vulnerable', 4: 'non vulnerable'})
 
-# Iterate through the float columns
+# Explore float columns using KDE plots
+plt.figure(figsize=(20, 16))
 for i, col in enumerate(train.select_dtypes('float')):
     ax = plt.subplot(4, 2, i + 1)
-    # Iterate through the poverty levels
     for poverty_level, color in colors.items():
-        # Plot each poverty level as a separate line
         sns.kdeplot(train.loc[train['Target'] == poverty_level, col].dropna(),
-                    ax = ax, color = color, label = poverty_mapping[poverty_level])
+                    ax=ax, color=color, label=poverty_mapping[poverty_level])
+    plt.title(f'{col.capitalize()} Distribution')
+    plt.xlabel(f'{col}')
+    plt.ylabel('Density')
+plt.subplots_adjust(top=2)
 
-    plt.title(f'{col.capitalize()} Distribution'); plt.xlabel(f'{col}'); plt.ylabel('Density')
-
-plt.subplots_adjust(top = 2)
-
-train.select_dtypes('object').head()
-
+# Mapping for 'yes' and 'no' values to 1 and 0 in certain columns
 mapping = {"yes": 1, "no": 0}
 
-# Apply same operation to both train and test
+# Apply the mapping to both train and test data
 for df in [train, test]:
-    # Fill in the values with the correct mapping
     df['dependency'] = df['dependency'].replace(mapping).astype(np.float64)
     df['edjefa'] = df['edjefa'].replace(mapping).astype(np.float64)
     df['edjefe'] = df['edjefe'].replace(mapping).astype(np.float64)
 
-train[['dependency', 'edjefa', 'edjefe']].describe()
-
-plt.figure(figsize = (16, 12))
-
-# Iterate through the float columns
+# Visualize the distribution of 'dependency', 'edjefa', and 'edjefe' columns
+plt.figure(figsize=(16, 12))
 for i, col in enumerate(['dependency', 'edjefa', 'edjefe']):
     ax = plt.subplot(3, 1, i + 1)
-    # Iterate through the poverty levels
     for poverty_level, color in colors.items():
-        # Plot each poverty level as a separate line
         sns.kdeplot(train.loc[train['Target'] == poverty_level, col].dropna(),
-                    ax = ax, color = color, label = poverty_mapping[poverty_level])
+                    ax=ax, color=color, label=poverty_mapping[poverty_level])
+    plt.title(f'{col.capitalize()} Distribution')
+    plt.xlabel(f'{col}')
+    plt.ylabel('Density')
+plt.subplots_adjust(top=2)
 
-    plt.title(f'{col.capitalize()} Distribution'); plt.xlabel(f'{col}'); plt.ylabel('Density')
-
-plt.subplots_adjust(top = 2)
-
-# Add null Target column to test
+# Add null 'Target' column to the test dataset
 test['Target'] = np.nan
 data = pd.concat([train, test], ignore_index=True)
 
-# Heads of household
+# Identify heads of households
 heads = data.loc[data['parentesco1'] == 1].copy()
 
 # Labels for training
 train_labels = data.loc[(data['Target'].notnull()) & (data['parentesco1'] == 1), ['Target', 'idhogar']]
 
-# Value counts of target
+# Visualize the distribution of poverty levels
 label_counts = train_labels['Target'].value_counts().sort_index()
+label_counts.plot.bar(figsize=(8, 6), color=colors.values(), edgecolor='k', linewidth=2)
+plt.xlabel('Poverty Level')
+plt.ylabel('Count')
+plt.xticks([x - 1 for x in poverty_mapping.keys()], list(poverty_mapping.values()), rotation=60)
+plt.title('Poverty Level Breakdown')
 
-# Bar plot of occurrences of each label
-label_counts.plot.bar(figsize = (8, 6),
-                      color = colors.values(),
-                      edgecolor = 'k', linewidth = 2)
-
-# Formatting
-plt.xlabel('Poverty Level'); plt.ylabel('Count');
-plt.xticks([x - 1 for x in poverty_mapping.keys()],
-           list(poverty_mapping.values()), rotation = 60)
-plt.title('Poverty Level Breakdown');
-
-label_counts
-
-# Groupby the household and figure out the number of unique values
+# Identify households where family members do not have the same target
 all_equal = train.groupby('idhogar')['Target'].apply(lambda x: x.nunique() == 1)
-
-# Households where targets are not all equal
 not_equal = all_equal[all_equal != True]
 print('There are {} households where the family members do not all have the same target.'.format(len(not_equal)))
 
-train[train['idhogar'] == not_equal.index[0]][['idhogar', 'parentesco1', 'Target']]
-
+# Handle households without a head
 households_leader = train.groupby('idhogar')['parentesco1'].sum()
-
-# Find households without a head
 households_no_head = train.loc[train['idhogar'].isin(households_leader[households_leader == 0].index), :]
-
 print('There are {} households without a head.'.format(households_no_head['idhogar'].nunique()))
 
-# Find households without a head and where labels are different
+# Check households without a head and different labels
 households_no_head_equal = households_no_head.groupby('idhogar')['Target'].apply(lambda x: x.nunique() == 1)
 print('{} Households with no head have different labels.'.format(sum(households_no_head_equal == False)))
 
-# Iterate through each household
+# Set the correct target label for each household
 for household in not_equal.index:
-    # Find the correct label (for the head of household)
-    true_target = int(train[(train['idhogar'] == household) & (train['parentesco1'] == 1.0)]['Target'])
-
-    # Set the correct label for all members in the household
+    true_target = int(train[(train['idhogar'] == household) & (train['parentesco1'] == 1.0)]['Target'].iloc[0])
     train.loc[train['idhogar'] == household, 'Target'] = true_target
 
-
-# Groupby the household and figure out the number of unique values
+# Check again for households where family members do not have the same target
 all_equal = train.groupby('idhogar')['Target'].apply(lambda x: x.nunique() == 1)
-
-# Households where targets are not all equal
 not_equal = all_equal[all_equal != True]
 print('There are {} households where the family members do not all have the same target.'.format(len(not_equal)))
 
-# Number of missing in each column
-missing = pd.DataFrame(data.isnull().sum()).rename(columns = {0: 'total'})
-
-# Create a percentage missing
+# Calculate and visualize missing values
+missing = pd.DataFrame(data.isnull().sum()).rename(columns={0: 'total'})
 missing['percent'] = missing['total'] / len(data)
+missing.sort_values('percent', ascending=False).head(10).drop('Target')
 
-missing.sort_values('percent', ascending = False).head(10).drop('Target')
-
-def plot_value_counts(df, col, heads_only = False):
-    """Plot value counts of a column, optionally with only the heads of a household"""
-    # Select heads of household
-    if heads_only:
-        df = df.loc[df['parentesco1'] == 1].copy()
-
-    plt.figure(figsize = (8, 6))
-    df[col].value_counts().sort_index().plot.bar(color = 'blue',
-                                                 edgecolor = 'k',
-                                                 linewidth = 2)
-    plt.xlabel(f'{col}'); plt.title(f'{col} Value Counts'); plt.ylabel('Count')
-    plt.show();
-
-    plot_value_counts(heads, 'v18q1')
-
-    heads.groupby('v18q')['v18q1'].apply(lambda x: x.isnull().sum())
-
-    data['v18q1'] = data['v18q1'].fillna(0)
-
-    # Variables indicating home ownership
-own_variables = [x for x in data if x.startswith('tipo')]
-
-
-# Plot of the home ownership variables for home missing rent payments
-data.loc[data['v2a1'].isnull(), own_variables].sum().plot.bar(figsize = (10, 8),
-                                                              color = 'green',
-                                                              edgecolor = 'k', linewidth = 2);
-plt.xticks([0, 1, 2, 3, 4],
-           ['Owns and Paid Off', 'Owns and Paying', 'Rented', 'Precarious', 'Other'],
-           rotation = 60)
-plt.title('Home Ownership Status for Households Missing Rent Payments', size = 18);
-
-# Fill in households that own the house with 0 rent payment
-data.loc[(data['tipovivi1'] == 1), 'v2a1'] = 0
-
-# Create missing rent payment column
-data['v2a1-missing'] = data['v2a1'].isnull()
-
-data['v2a1-missing'].value_counts()
-
-data.loc[data['rez_esc'].notnull()]['age'].describe()
-
-data.loc[data['rez_esc'].isnull()]['age'].describe()
-
-# If individual is over 19 or younger than 7 and missing years behind, set it to 0
-data.loc[((data['age'] > 19) | (data['age'] < 7)) & (data['rez_esc'].isnull()), 'rez_esc'] = 0
-
-# Add a flag for those between 7 and 19 with a missing value
-data['rez_esc-missing'] = data['rez_esc'].isnull()
-
-data.loc[data['rez_esc'] > 5, 'rez_esc'] = 5
-
-def plot_categoricals(x, y, data, annotate = True):
-    """Plot counts of two categoricals.
-    Size is raw count for each grouping.
-    Percentages are for a given value of y."""
-
-    # Raw counts
-    raw_counts = pd.DataFrame(data.groupby(y)[x].value_counts(normalize = False))
-    raw_counts = raw_counts.rename(columns = {x: 'raw_count'})
-
-    # Calculate counts for each group of x and y
-    counts = pd.DataFrame(data.groupby(y)[x].value_counts(normalize = True))
-
-    # Rename the column and reset the index
-    counts = counts.rename(columns = {x: 'normalized_count'}).reset_index()
+# Define a function to plot counts of two categoricals
+def plot_categoricals(x, y, data, annotate=True):
+    raw_counts = pd.DataFrame(data.groupby(y)[x].value_counts(normalize=False))
+    raw_counts = raw_counts.rename(columns={x: 'raw_count'})
+    counts = pd.DataFrame(data.groupby(y)[x].value_counts(normalize=True))
+    counts = counts.rename(columns={x: 'normalized_count'}).reset_index()
     counts['percent'] = 100 * counts['normalized_count']
-
-    # Add the raw count
     counts['raw_count'] = list(raw_counts['raw_count'])
 
-    plt.figure(figsize = (14, 10))
-    # Scatter plot sized by percent
-    plt.scatter(counts[x], counts[y], edgecolor = 'k', color = 'lightgreen',
-                s = 100 * np.sqrt(counts['raw_count']), marker = 'o',
-                alpha = 0.6, linewidth = 1.5)
+    plt.figure(figsize=(14, 10))
+    plt.scatter(counts[x], counts[y], edgecolor='k', color='lightgreen',
+                s=100 * np.sqrt(counts['raw_count']), marker='o',
+                alpha=0.6, linewidth=1.5)
 
     if annotate:
-        # Annotate the plot with text
         for i, row in counts.iterrows():
-            # Put text with appropriate offsets
-            plt.annotate(xy = (row[x] - (1 / counts[x].nunique()),
-                               row[y] - (0.15 / counts[y].nunique())),
-                         color = 'navy',
-                         s = f"{round(row['percent'], 1)}%")
+            plt.annotate(xy=(row[x] - (1 / counts[x].nunique()), row[y] - (0.15 / counts[y].nunique())),
+                         color='navy', s=f"{round(row['percent'], 1)}%")
 
-    # Set tick marks
     plt.yticks(counts[y].unique())
     plt.xticks(counts[x].unique())
 
-    # Transform min and max to evenly space in square root domain
     sqr_min = int(np.sqrt(raw_counts['raw_count'].min()))
     sqr_max = int(np.sqrt(raw_counts['raw_count'].max()))
-
-    # 5 sizes for legend
-    msizes = list(range(sqr_min, sqr_max,
-                        int(( sqr_max - sqr_min) / 5)))
+    msizes = list(range(sqr_min, sqr_max, int((sqr_max - sqr_min) / 5)))
     markers = []
 
-    # Markers for legend
     for size in msizes:
-        markers.append(plt.scatter([], [], s = 100 * size,
-                                   label = f'{int(round(np.square(size) / 100) * 100)}',
-                                   color = 'lightgreen',
-                                   alpha = 0.6, edgecolor = 'k', linewidth = 1.5))
+        markers.append(plt.scatter([], [], s=100 * size,
+                                   label=f'{int(round(np.square(size) / 100) * 100)}',
+                                   color='lightgreen', alpha=0.6, edgecolor='k', linewidth=1.5))
 
-    # Legend and formatting
-    plt.legend(handles = markers, title = 'Counts',
-               labelspacing = 3, handletextpad = 2,
-               fontsize = 16,
-               loc = (1.10, 0.19))
+    plt.legend(handles=markers, title='Counts', labelspacing=3, handletextpad=2,
+               fontsize=16, loc=(1.10, 0.19))
 
-    plt.annotate(f'* Size represents raw count while % is for a given y value.',
-                 xy = (0, 1), xycoords = 'figure points', size = 10)
+    plt.annotate(f'* Size represents the raw count while % is for a given y value.',
+                 xy=(0, 1), xycoords='figure points', size=10)
 
-    # Adjust axes limits
-    plt.xlim((counts[x].min() - (6 / counts[x].nunique()),
-              counts[x].max() + (6 / counts[x].nunique())))
-    plt.ylim((counts[y].min() - (4 / counts[y].nunique()),
-              counts[y].max() + (4 / counts[y].nunique())))
+    plt.xlim((counts[x].min() - (6 / counts[x].nunique()), counts[x].max() + (6 / counts[x].nunique())))
+    plt.ylim((counts[y].min() - (4 / counts[y].nunique()), counts[y].max() + (4 / counts[y].nunique())))
     plt.grid(None)
-    plt.xlabel(f"{x}"); plt.ylabel(f"{y}"); plt.title(f"{y} vs {x}");
+    plt.xlabel(f"{x}")
+    plt.ylabel(f"{y}")
+    plt.title(f"{y} vs {x}")
 
-    plot_categoricals('rez_esc', 'Target', data);
+# Example: Displaying the categorical strip plot
+category1 = 'rez_esc'
+category2 = 'Target'
+
+# Create a categorical strip plot
+sns.catplot(x=category1, y=category2, data=train, kind='strip')
+plt.show()
